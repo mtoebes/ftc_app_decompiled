f2f21c68f51497a43d0db8491c23ae3c6d1d1b72
mtoebes <margaret.toebes@gmail.com>
2015-12-05 00:01:39 -0500

    refactor RobocolParsable

--- FtcRobotController/src/main/java/com/qualcomm/robotcore/robocol/RobocolParsable.java
+++ FtcRobotController/src/main/java/com/qualcomm/robotcore/robocol/RobocolParsable.java
@@ -4,8 +4,8 @@ import com.qualcomm.robotcore.exception.RobotCoreException;
 import com.qualcomm.robotcore.util.RobotLog;
 
 public interface RobocolParsable {
-    public static final byte[] EMPTY_HEADER_BUFFER;
     public static final int HEADER_LENGTH = 3;
+    public static final byte[] EMPTY_HEADER_BUFFER = new byte[HEADER_LENGTH];;
 
     public enum MsgType {
         EMPTY(0),
@@ -15,29 +15,25 @@ public interface RobocolParsable {
         COMMAND(4),
         TELEMETRY(5);
         
-        private static final MsgType[] f335a;
-        private final int f337b;
-
-        static {
-            f335a = values();
-        }
+        private static final MsgType[] messageTypes = values();
+        private final int type;
 
         public static MsgType fromByte(byte b) {
             MsgType msgType = EMPTY;
             try {
-                return f335a[b];
+                return messageTypes[b];
             } catch (ArrayIndexOutOfBoundsException e) {
-                RobotLog.w(String.format("Cannot convert %d to MsgType: %s", new Object[]{Byte.valueOf(b), e.toString()}));
+                RobotLog.w(String.format("Cannot convert %d to MsgType: %s", b, e.toString()));
                 return msgType;
             }
         }
 
         private MsgType(int type) {
-            this.f337b = type;
+            this.type = type;
         }
 
         public byte asByte() {
-            return (byte) this.f337b;
+            return (byte) this.type;
         }
     }
 
@@ -46,8 +42,4 @@ public interface RobocolParsable {
     MsgType getRobocolMsgType();
 
     byte[] toByteArray() throws RobotCoreException;
-
-    static {
-        EMPTY_HEADER_BUFFER = new byte[HEADER_LENGTH];
-    }
 }
