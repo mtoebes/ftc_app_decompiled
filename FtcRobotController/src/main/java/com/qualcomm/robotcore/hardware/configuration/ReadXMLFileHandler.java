70f4c98612f3a8520df60ba026771d9b79cbb363
mtoebes <margaret.toebes@gmail.com>
2015-12-06 14:50:27 -0500

    add todo, being working on this too long

--- FtcRobotController/src/main/java/com/qualcomm/robotcore/hardware/configuration/ReadXMLFileHandler.java
+++ FtcRobotController/src/main/java/com/qualcomm/robotcore/hardware/configuration/ReadXMLFileHandler.java
@@ -12,11 +12,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
+
+
+// TODO come back, lots of optimization to finish
 
 public class ReadXMLFileHandler {
     private static boolean DEBUG = false;
