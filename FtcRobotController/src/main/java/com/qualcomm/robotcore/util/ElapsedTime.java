86c0e44add891b97a375e57d441d8ad85df6fd43
mtoebes <margaret.toebes@gmail.com>
2015-12-06 19:32:15 -0500

    ElapseTime

--- FtcRobotController/src/main/java/com/qualcomm/robotcore/util/ElapsedTime.java
+++ FtcRobotController/src/main/java/com/qualcomm/robotcore/util/ElapsedTime.java
@@ -1,27 +1,11 @@
 package com.qualcomm.robotcore.util;
 
-import com.ftdi.j2xx.protocol.SpiSlaveResponseEvent;
-
 public class ElapsedTime {
-    private long f377a;
-    private double f378b;
+    private static final double TIME_IN_MILLI = 1.0E6d;
+    private static final double TIME_IN_NANO = 1.0E9d;
 
-    /* renamed from: com.qualcomm.robotcore.util.ElapsedTime.1 */
-    static /* synthetic */ class C00471 {
-        static final /* synthetic */ int[] f375a;
-
-        static {
-            f375a = new int[Resolution.values().length];
-            try {
-                f375a[Resolution.SECONDS.ordinal()] = 1;
-            } catch (NoSuchFieldError e) {
-            }
-            try {
-                f375a[Resolution.MILLISECONDS.ordinal()] = 2;
-            } catch (NoSuchFieldError e2) {
-            }
-        }
-    }
+    private long startTime = 0;
+    private double timeResolution = TIME_IN_NANO;
 
     public enum Resolution {
         SECONDS,
@@ -29,57 +13,51 @@ public class ElapsedTime {
     }
 
     public ElapsedTime() {
-        this.f377a = 0;
-        this.f378b = 1.0E9d;
         reset();
     }
 
     public ElapsedTime(long startTime) {
-        this.f377a = 0;
-        this.f378b = 1.0E9d;
-        this.f377a = startTime;
+        this.startTime = startTime;
     }
 
     public ElapsedTime(Resolution resolution) {
-        this.f377a = 0;
-        this.f378b = 1.0E9d;
         reset();
-        switch (C00471.f375a[resolution.ordinal()]) {
-            case SpiSlaveResponseEvent.DATA_CORRUPTED /*1*/:
-                this.f378b = 1.0E9d;
-            case SpiSlaveResponseEvent.IO_ERROR /*2*/:
-                this.f378b = 1000000.0d;
+        switch (resolution) {
+            case SECONDS :
+                timeResolution = TIME_IN_NANO;
+            case MILLISECONDS :
+                timeResolution = TIME_IN_MILLI;
             default:
         }
     }
 
     public void reset() {
-        this.f377a = System.nanoTime();
+        this.startTime = System.nanoTime();
     }
 
     public double startTime() {
-        return ((double) this.f377a) / this.f378b;
+        return ((double) startTime) / timeResolution;
     }
 
     public double time() {
-        return ((double) (System.nanoTime() - this.f377a)) / this.f378b;
+        return ((double) (System.nanoTime() - startTime)) / timeResolution;
     }
 
-    private String m219a() {
-        if (this.f378b == 1.0E9d) {
+    private String timeResolutionToString() {
+        if (timeResolution == TIME_IN_NANO) {
             return "seconds";
-        }
-        if (this.f378b == 1000000.0d) {
+        } else if (timeResolution == TIME_IN_MILLI) {
             return "milliseconds";
+        } else {
+            return "Unknown units";
         }
-        return "Unknown units";
     }
 
     public void log(String label) {
-        RobotLog.v(String.format("TIMER: %20s - %1.3f %s", new Object[]{label, Double.valueOf(time()), m219a()}));
+        RobotLog.v(String.format("TIMER: %20s - %1.3f %s", label, time(), timeResolutionToString()));
     }
 
     public String toString() {
-        return String.format("%1.4f %s", new Object[]{Double.valueOf(time()), m219a()});
+        return String.format("%1.4f %s", time(), timeResolutionToString());
     }
 }
