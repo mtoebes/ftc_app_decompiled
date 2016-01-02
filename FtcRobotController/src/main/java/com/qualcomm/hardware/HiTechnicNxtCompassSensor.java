ec027135766dbaff5fcaf3213d76c6fa1f4866f6
mtoebes <margaret.toebes@gmail.com>
2016-01-02 15:10:50 -0500

    naming refactor

--- FtcRobotController/src/main/java/com/qualcomm/hardware/HiTechnicNxtCompassSensor.java
+++ FtcRobotController/src/main/java/com/qualcomm/hardware/HiTechnicNxtCompassSensor.java
@@ -1,7 +1,6 @@
 package com.qualcomm.hardware;
 
 import com.qualcomm.robotcore.hardware.CompassSensor;
-import com.qualcomm.robotcore.hardware.CompassSensor.CompassMode;
 import com.qualcomm.robotcore.hardware.I2cController.I2cPortReadyCallback;
 import com.qualcomm.robotcore.util.TypeConversion;
 import java.nio.ByteOrder;
@@ -22,99 +21,93 @@ public class HiTechnicNxtCompassSensor extends CompassSensor implements I2cPortR
     public static final byte MEASUREMENT = (byte) 0;
     public static final byte MODE_CONTROL_ADDRESS = (byte) 65;
     public static final byte ONE_DEGREE_HEADING_ADDER = (byte) 67;
-    private final ModernRoboticsUsbLegacyModule f34a;
-    private final byte[] f35b;
-    private final Lock f36c;
-    private final byte[] f37d;
-    private final Lock f38e;
-    private final int f39f;
-    private CompassMode f40g;
-    private boolean f41h;
+
+    private static final byte OFFSET_COMMAND = 3;
+
+    private final ModernRoboticsUsbLegacyModule legacyModule;
+    private final byte[] readCache;
+    private final Lock readCacheLock;
+    private final byte[] writeCache;
+    private final Lock writeCacheLock;
+    private final int physicalPort;
+    private CompassMode compassMode = CompassMode.MEASUREMENT_MODE;
+    private boolean settingMode;
 
     public HiTechnicNxtCompassSensor(ModernRoboticsUsbLegacyModule legacyModule, int physicalPort) {
-        this.f40g = CompassMode.MEASUREMENT_MODE;
-        this.f41h = false;
-        legacyModule.enableI2cReadMode(physicalPort, HEADING_WORD_LENGTH, COMPASS_BUFFER, COMPASS_BUFFER_SIZE);
-        this.f34a = legacyModule;
-        this.f35b = legacyModule.getI2cReadCache(physicalPort);
-        this.f36c = legacyModule.getI2cReadCacheLock(physicalPort);
-        this.f37d = legacyModule.getI2cWriteCache(physicalPort);
-        this.f38e = legacyModule.getI2cWriteCacheLock(physicalPort);
-        this.f39f = physicalPort;
+        legacyModule.enableI2cReadMode(physicalPort, I2C_ADDRESS, COMPASS_BUFFER, COMPASS_BUFFER_SIZE);
+        this.legacyModule = legacyModule;
+        this.readCache = legacyModule.getI2cReadCache(physicalPort);
+        this.readCacheLock = legacyModule.getI2cReadCacheLock(physicalPort);
+        this.writeCache = legacyModule.getI2cWriteCache(physicalPort);
+        this.writeCacheLock = legacyModule.getI2cWriteCacheLock(physicalPort);
+        this.physicalPort = physicalPort;
         legacyModule.registerForI2cPortReadyCallback(this, physicalPort);
     }
 
     public double getDirection() {
-        if (this.f41h || this.f40g == CompassMode.CALIBRATION_MODE) {
+        if (this.settingMode || this.compassMode == CompassMode.CALIBRATION_MODE) {
             return INVALID_DIRECTION;
         }
         try {
-            this.f36c.lock();
-            byte[] copyOfRange = Arrays.copyOfRange(this.f35b, 7, 9);
+            this.readCacheLock.lock();
+            byte[] copyOfRange = Arrays.copyOfRange(this.readCache, DIRECTION_START, DIRECTION_END);
             return (double) TypeConversion.byteArrayToShort(copyOfRange, ByteOrder.LITTLE_ENDIAN);
         } finally {
-            this.f36c.unlock();
+            this.readCacheLock.unlock();
         }
     }
 
     public String status() {
-        Object[] objArr = new Object[HEADING_WORD_LENGTH];
-        objArr[0] = this.f34a.getSerialNumber().toString();
-        objArr[1] = this.f39f;
-        return String.format("NXT Compass Sensor, connected via device %s, port %d", objArr);
+        Object[] stateBuffer = new Object[HEADING_WORD_LENGTH];
+        stateBuffer[0] = this.legacyModule.getSerialNumber().toString();
+        stateBuffer[1] = this.physicalPort;
+        return String.format("NXT Compass Sensor, connected via device %s, port %d", stateBuffer);
     }
 
     public void setMode(CompassMode mode) {
-        if (this.f40g != mode) {
-            this.f40g = mode;
-            m38a();
-        }
-    }
+        if (this.compassMode != mode) {
+            this.compassMode = mode;
+            this.settingMode = true;
 
-    private void m38a() {
-        this.f41h = true;
-        byte b = this.f40g == CompassMode.CALIBRATION_MODE ? ONE_DEGREE_HEADING_ADDER : MEASUREMENT;
-        this.f34a.enableI2cWriteMode(this.f39f, HEADING_WORD_LENGTH, COMPASS_BUFFER, 1);
-        try {
-            this.f38e.lock();
-            this.f37d[3] = b;
-        } finally {
-            this.f38e.unlock();
+            byte compassModeByte = (this.compassMode == CompassMode.CALIBRATION_MODE ? CALIBRATION : MEASUREMENT);
+            this.legacyModule.enableI2cWriteMode(this.physicalPort, I2C_ADDRESS, COMPASS_BUFFER, 1);
+            try {
+                this.writeCacheLock.lock();
+                this.writeCache[OFFSET_COMMAND] = compassModeByte;
+            } finally {
+                this.writeCacheLock.unlock();
+            }
         }
-    }
-
-    private void m39b() {
-        if (this.f40g == CompassMode.MEASUREMENT_MODE) {
-            this.f34a.enableI2cReadMode(this.f39f, HEADING_WORD_LENGTH, COMPASS_BUFFER, COMPASS_BUFFER_SIZE);
-        }
-        this.f41h = false;
     }
 
     public boolean calibrationFailed() {
-        boolean z = false;
-        if (!(this.f40g == CompassMode.CALIBRATION_MODE || this.f41h)) {
+        boolean calibrationFailed = false;
+        if (!(this.compassMode == CompassMode.CALIBRATION_MODE || this.settingMode)) {
             try {
-                this.f36c.lock();
-                if (this.f35b[3] == 70) {
-                    z = true;
+                this.readCacheLock.lock();
+                if (this.readCache[OFFSET_COMMAND] == CALIBRATION_FAILURE) {
+                    calibrationFailed = true;
                 }
-                this.f36c.unlock();
+                this.readCacheLock.unlock();
             } catch (Throwable th) {
-                this.f36c.unlock();
+                this.readCacheLock.unlock();
             }
         }
-        return z;
+        return calibrationFailed;
     }
 
     public void portIsReady(int port) {
-        this.f34a.setI2cPortActionFlag(this.f39f);
-        this.f34a.readI2cCacheFromController(this.f39f);
-        if (this.f41h) {
-            m39b();
-            this.f34a.writeI2cCacheToController(this.f39f);
+        this.legacyModule.setI2cPortActionFlag(this.physicalPort);
+        this.legacyModule.readI2cCacheFromController(this.physicalPort);
+        if (this.settingMode) {
+            if (this.compassMode == CompassMode.MEASUREMENT_MODE) {
+                this.legacyModule.enableI2cReadMode(this.physicalPort, I2C_ADDRESS, COMPASS_BUFFER, COMPASS_BUFFER_SIZE);
+            }
+            this.settingMode = false;
+            this.legacyModule.writeI2cCacheToController(this.physicalPort);
             return;
         }
-        this.f34a.writeI2cPortFlagOnlyToController(this.f39f);
+        this.legacyModule.writeI2cPortFlagOnlyToController(this.physicalPort);
     }
 
     public String getDeviceName() {
@@ -122,7 +115,7 @@ public class HiTechnicNxtCompassSensor extends CompassSensor implements I2cPortR
     }
 
     public String getConnectionInfo() {
-        return this.f34a.getConnectionInfo() + "; port " + this.f39f;
+        return this.legacyModule.getConnectionInfo() + "; port " + this.physicalPort;
     }
 
     public int getVersion() {
