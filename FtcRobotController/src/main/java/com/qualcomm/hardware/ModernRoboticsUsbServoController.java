24c34c76e54f4bfbf738e0ef5fc061de3522caf1
mtoebes <margaret.toebes@gmail.com>
2016-01-03 01:42:01 -0500

    naming refactor

--- FtcRobotController/src/main/java/com/qualcomm/hardware/ModernRoboticsUsbServoController.java
+++ FtcRobotController/src/main/java/com/qualcomm/hardware/ModernRoboticsUsbServoController.java
@@ -3,37 +3,27 @@ package com.qualcomm.hardware;
 import com.qualcomm.robotcore.eventloop.EventLoopManager;
 import com.qualcomm.robotcore.exception.RobotCoreException;
 import com.qualcomm.robotcore.hardware.ServoController;
-import com.qualcomm.robotcore.hardware.ServoController.PwmStatus;
 import com.qualcomm.robotcore.hardware.usb.RobotUsbDevice;
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.util.SerialNumber;
 import com.qualcomm.robotcore.util.TypeConversion;
 
 public class ModernRoboticsUsbServoController extends ModernRoboticsUsbDevice implements ServoController {
-    public static final int ADDRESS_CHANNEL1 = 66;
-    public static final int ADDRESS_CHANNEL2 = 67;
-    public static final int ADDRESS_CHANNEL3 = 68;
-    public static final int ADDRESS_CHANNEL4 = 69;
-    public static final int ADDRESS_CHANNEL5 = 70;
-    public static final int ADDRESS_CHANNEL6 = 71;
-    public static final byte[] ADDRESS_CHANNEL_MAP;
+
     public static final int ADDRESS_PWM = 72;
     public static final int ADDRESS_UNUSED = -1;
     public static final boolean DEBUG_LOGGING = false;
-    public static final int MAX_SERVOS = 6;
+    public static final int NUMBER_OF_SERVOS = 6;
     public static final int MONITOR_LENGTH = 9;
+
     public static final byte PWM_DISABLE = (byte) -1;
     public static final byte PWM_ENABLE = (byte) 0;
-    public static final byte PWM_ENABLE_WITHOUT_TIMEOUT = (byte) -86;
+
     public static final int SERVO_POSITION_MAX = 255;
     public static final byte START_ADDRESS = (byte) 64;
 
-    static {
-        ADDRESS_CHANNEL_MAP = new byte[]{PWM_DISABLE, HiTechnicNxtCompassSensor.HEADING_IN_TWO_DEGREE_INCREMENTS, HiTechnicNxtCompassSensor.ONE_DEGREE_HEADING_ADDER, (byte) 68, (byte) 69, HiTechnicNxtCompassSensor.CALIBRATION_FAILURE, (byte) 71};
-    }
-
     protected ModernRoboticsUsbServoController(SerialNumber serialNumber, RobotUsbDevice device, EventLoopManager manager) throws RobotCoreException, InterruptedException {
-        super(serialNumber, manager, new ReadWriteRunnableBlocking(serialNumber, device, MONITOR_LENGTH, 64, DEBUG_LOGGING));
+        super(serialNumber, manager, new ReadWriteRunnableBlocking(serialNumber, device, MONITOR_LENGTH, START_ADDRESS, DEBUG_LOGGING));
         pwmDisable();
     }
 
@@ -66,20 +56,24 @@ public class ModernRoboticsUsbServoController extends ModernRoboticsUsbDevice im
     }
 
     public void setServoPosition(int channel, double position) {
-        m72a(channel);
+        validateChannel(channel);
         Range.throwIfRangeIsInvalid(position, 0.0d, 1.0d);
-        write((int) ADDRESS_CHANNEL_MAP[channel], 255.0d * position);
+        write(getChannelAddress(channel), SERVO_POSITION_MAX * position);
         pwmEnable();
     }
 
     public double getServoPosition(int channel) {
-        m72a(channel);
-        return TypeConversion.unsignedByteToDouble(read(ADDRESS_CHANNEL_MAP[channel], 1)[0]) / 255.0d;
+        validateChannel(channel);
+        return TypeConversion.unsignedByteToDouble(read(getChannelAddress(channel), 1)[0]) / SERVO_POSITION_MAX;
     }
 
-    private void m72a(int i) {
-        if (i < 1 || i > ADDRESS_CHANNEL_MAP.length) {
-            throw new IllegalArgumentException(String.format("Channel %d is invalid; valid channels are 1..%d", new Object[]{i, MAX_SERVOS}));
+    private void validateChannel(int port) {
+        if (port < 1 || port >= NUMBER_OF_SERVOS) {
+            throw new IllegalArgumentException(String.format("Channel %d is invalid; valid channels are 1..%d", port, NUMBER_OF_SERVOS-1));
         }
+    }
+
+    private int getChannelAddress(int channel) {
+        return channel + 65;
     }
 }
