be42a62bda75ba8b822a102cdcc3abb15c3005ae
mtoebes <margaret.toebes@gmail.com>
2016-01-02 19:21:24 -0500

    group constants that share a value

--- FtcRobotController/src/main/java/com/qualcomm/hardware/ModernRoboticsUsbLegacyModule.java
+++ FtcRobotController/src/main/java/com/qualcomm/hardware/ModernRoboticsUsbLegacyModule.java
@@ -10,31 +10,36 @@ import java.util.concurrent.locks.Lock;
 
 public class ModernRoboticsUsbLegacyModule extends ModernRoboticsUsbDevice implements LegacyModule {
 
-    public static final int ADDRESS_BUFFER_STATUS = 3;
-
-    public static final boolean DEBUG_LOGGING = false;
-    public static final byte I2C_ACTION_FLAG = (byte) -1;
     public static final byte I2C_NO_ACTION_FLAG = (byte) 0;
-    public static final byte MAX_PORT_NUMBER = (byte) 5;
     public static final byte MIN_PORT_NUMBER = (byte) 0;
+    public static final byte NXT_MODE_ANALOG = (byte) 0;
+    public static final byte NXT_MODE_WRITE = (byte) 0;
+    public static final byte OFFSET_I2C_PORT_MODE = (byte) 0;
+
+    public static final byte NXT_MODE_I2C = (byte) 1;
+    public static final byte OFFSET_I2C_PORT_I2C_ADDRESS = (byte) 1;
+
+    public static final byte NXT_MODE_9V_ENABLED = (byte) 2;
+    public static final byte OFFSET_I2C_PORT_MEMORY_ADDRESS = (byte) 2;
+    public static final byte SIZE_ANALOG_BUFFER = (byte) 2;
+
+    public static final int ADDRESS_BUFFER_STATUS = 3;
+    public static final byte OFFSET_I2C_PORT_MEMORY_LENGTH = (byte) 3;
+    public static final byte START_ADDRESS = (byte) 3;
+
+    public static final byte I2C_ACTION_FLAG = (byte) -1;
+    public static final byte MAX_PORT_NUMBER = (byte) 5;
     public static final int MONITOR_LENGTH = 13;
     public static final byte NUMBER_OF_PORTS = (byte) 6;
-    public static final byte NXT_MODE_9V_ENABLED = (byte) 2;
-    public static final byte NXT_MODE_ANALOG = (byte) 0;
-    public static final byte NXT_MODE_I2C = (byte) 1;
     public static final byte NXT_MODE_READ = -127;
-    public static final byte NXT_MODE_WRITE = (byte) 0;
     public static final byte OFFSET_I2C_PORT_FLAG = (byte) 31;
-    public static final byte OFFSET_I2C_PORT_I2C_ADDRESS = (byte) 1;
-    public static final byte OFFSET_I2C_PORT_MEMORY_ADDRESS = (byte) 2;
     public static final byte OFFSET_I2C_PORT_MEMORY_BUFFER = (byte) 4;
-    public static final byte OFFSET_I2C_PORT_MEMORY_LENGTH = (byte) 3;
-    public static final byte OFFSET_I2C_PORT_MODE = (byte) 0;
     public static final int[] PORT_9V_CAPABLE = new int[]{4, 5};
-    public static final byte SIZE_ANALOG_BUFFER = (byte) 2;
     public static final byte SIZE_I2C_BUFFER = (byte) 27;
     public static final byte SIZE_OF_PORT_BUFFER = (byte) 32;
-    public static final byte START_ADDRESS = (byte) 3;
+
+    public static final boolean DEBUG_LOGGING = false;
+
     private final ReadWriteRunnableSegment[] segments;
     private final I2cPortReadyCallback[] callbacks;
 
